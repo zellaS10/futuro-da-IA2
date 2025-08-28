91d040a39e5b333cf1de44b8f0e5cba15131165e
zellaS10 <paulorafaelzella1@outlook.com>
2025-08-28 11:52:46 -0300

    ..

--- script.js
+++ script.js
@@ -2,4 +2,27 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const TextoResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: " Você já ouviu falar em inteligencia artificial? "
+        Alternativas; [
+            "Sim"
+            "Não"
+        ]
+    },
+    {
+        enunciado: "Você acha que a AI facillita o aprendizado?"
+     alternativas: [
+        "Sim"
+        "Não"
+    ]
+},
+{
+    enunciado: "Pergunta 3"
+    alternativas: [
+        "Alternativa 1"
+        "Alternativa 2"
+    ]
+},
